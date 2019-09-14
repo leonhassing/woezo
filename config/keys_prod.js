093c39421217d3c3fda00b710fa6648594b7f4af
Leon Hassing <hassingleon@gmail.com>
2019-09-14 11:26:24 +0200

    adjust mongo uri

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  secretOrKey: process.env.SECRET_OR_KEY
+  mongoURI: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com/',
+  secretOrKey: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com/'
 };
