8a741066d96b4237ff39ee7daf4df8c98e3167aa
Leon Hassing <hassingleon@gmail.com>
2019-09-14 11:49:38 +0200

    added public ip to mongodb connection string

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin',
-  secretOrKey: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin'
+  mongoURI: 'mongodb://15.188.8.64:27017/woezoDB?authSource=admin',
+  secretOrKey: 'mongodb://15.188.8.64:27017/woezoDB?authSource=admin'
 };
