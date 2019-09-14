cff99a3050a3678867629686b855e2f1c2f66afb
Leon Hassing <hassingleon@gmail.com>
2019-09-14 14:14:44 +0200

    changed mongodb uri

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://leonhassing:ilovecats@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB',
-  secretOrKey: 'mongodb://leonhassing:ilovecats@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB'
+  mongoURI: 'mongodb://god:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin',
+  secretOrKey: 'mongodb://god:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin'
 };
