0e1042991baf8611bd2249129c22ee116866b737
Leon Hassing <hassingleon@gmail.com>
2019-09-14 11:46:04 +0200

    added ?authSource=admin

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB/',
-  secretOrKey: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB/'
+  mongoURI: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin',
+  secretOrKey: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin'
 };
