82fc7d85242e5b6e9b9ba6f14f1530c59ab14edb
Leon Hassing <hassingleon@gmail.com>
2019-09-14 12:31:53 +0200

    change mongodb connection back to public IP

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin',
-  secretOrKey: 'mongodb://admin:admin123@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB?authSource=admin'
+  mongoURI: 'mongodb://admin:admin123@15.188.8.64:27017/woezoDB?authSource=admin',
+  secretOrKey: 'mongodb://admin:admin123@15.188.8.64:27017/woezoDB?authSource=admin'
 };
