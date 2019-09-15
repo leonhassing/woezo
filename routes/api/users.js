39281974aad5f31e8575ed3cbe249159332d266d
Leon Hassing <hassingleon@gmail.com>
2019-09-15 17:17:37 +0200

    change register user api

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,16 +47,13 @@ router.post('/register', (req, res) => {
         password: req.body.password
       });
 
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
+      if (err) throw err;
+      
+      newUser.password = hash;
+      newUser
+        .save()
+        .then(user => res.json(user))
+        .catch(err => console.log(err));
     }
   });
 });
