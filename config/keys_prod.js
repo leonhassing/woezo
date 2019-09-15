11fafd5a4d6fa28705c20fd14ca16faf4cfcdcc8
Leon Hassing <hassingleon@gmail.com>
2019-09-15 18:02:58 +0200

    adjust mongo URI

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://god:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/DB?authSource=admin',
-  secretOrKey: 'mongodb://god:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/DB?authSource=admin'
+  mongoURI: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/DB',
+  secretOrKey: 'mongodb://ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/DB'
 };
