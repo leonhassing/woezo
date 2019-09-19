8ebb392b5a442ad59d6d0d95bb684cc7ee267a96
Leon Hassing <hassingleon@gmail.com>
2019-09-19 17:02:52 +0200

    changed password

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://automationaccount:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB',
-  secretOrKey: 'mongodb://automationaccount:zJp3fp5LarvHUXFH@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB'
+  mongoURI: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB',
+  secretOrKey: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB'
 };
