2637abe2d93454f2a511f308e348ff43b24abc8a
Leon Hassing <hassingleon@gmail.com>
2019-09-23 21:35:30 +0200

    revert users api

--- routes/api/users.js
+++ routes/api/users.js
@@ -29,26 +29,36 @@ router.post('/register', (req, res) => {
     return res.status(400).json(errors);
   }
 
-  const avatar = gravatar.url(req.body.email, {
-    s: '200', // Size
-    r: 'pg', // Rating
-    d: 'mm' // Default
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', // Size
+        r: 'pg', // Rating
+        d: 'mm' // Default
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
   });
-
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    avatar,
-    password: req.body.password
-  });
-
-  newUser
-    .save()
-    .then(user => res.json(user))
-    .catch(err => console.log(err));
-
-  return res.status(400).json("reached the end");
-
 });
 
 // @route   GET api/users/login
