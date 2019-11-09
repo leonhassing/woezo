277f3680f6608c3fb3d9d8be9dd5e2eeef5ecea1
Leon Hassing <hassingleon@gmail.com>
2019-11-09 16:17:40 +0100

    update database key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB',
-  secretOrKey: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-15-188-8-64.eu-west-3.compute.amazonaws.com:27017/woezoDB'
+  mongoURI: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-35-181-153-199.eu-west-3.compute.amazonaws.com:27017/woezoDB',
+  secretOrKey: 'mongodb://automationaccount:EdgnQkysZEcyRVRX@ec2-35-181-153-199.eu-west-3.compute.amazonaws.com:27017/woezoDB'
 };
