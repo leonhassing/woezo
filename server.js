2a3f3535645d17d1290f34a78fffc2f54b0bc6dd
Leon Hassing <hassingleon@gmail.com>
2019-12-20 18:37:35 +0100

    changed CORS origin to woezo..nl

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const posts = require('./routes/api/posts');
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "https://woezo.nl");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, authorization");
   next();
 });
